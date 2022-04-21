60a6934f439e1fea95c9497ca945cfc3c40c2938
Zilooow <97363771+Zilooow@users.noreply.github.com>
2022-04-21 21:25:10 +0200

    Delete public directory

--- public/js/image.js
+++
@@ -1,3 +0,0 @@
-document.oncontextmenu=new Function("return false")
-document.ondragstart =new Function("return false")
-function ImpEcrOff() { SetInterval("window.clipboardData.setData('text','')",20); }
